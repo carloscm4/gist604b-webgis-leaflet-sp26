abd5826d97d05c4b2ef44c739900946e359f6e93
Carlos Castillo <carloscm4@arizona.edu>
2026-05-03 11:21:54 -0400

    roads data

---
+++ data/road.js
@@ -0,0 +1,39 @@
+var roadsData = {
+"type": "FeatureCollection",
+"features": [
+{
+"type": "Feature",
+"properties": { "name": "I-95" },
+"geometry": {
+"type": "LineString",
+"coordinates": [
+[-81.70, 30.40],
+[-81.66, 30.30],
+[-81.63, 30.20]
+]
+}
+},
+{
+"type": "Feature",
+"properties": { "name": "I-10" },
+"geometry": {
+"type": "LineString",
+"coordinates": [
+[-81.85, 30.35],
+[-81.50, 30.35]
+]
+}
+},
+{
+"type": "Feature",
+"properties": { "name": "US-17" },
+"geometry": {
+"type": "LineString",
+"coordinates": [
+[-81.70, 30.30],
+[-81.70, 30.10]
+]
+}
+}
+]
+};
