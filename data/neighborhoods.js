94f2bae576637f1d13ea5e40ddc041dbad1ebd69
Carlos Castillo <carloscm4@arizona.edu>
2026-05-03 11:22:37 -0400

    neighborhoods data

---
+++ data/neighborhoods.js
@@ -0,0 +1,47 @@
+var neighborhoodsData = {
+"type": "FeatureCollection",
+"features": [
+{
+"type": "Feature",
+"properties": { "name": "Downtown" },
+"geometry": {
+"type": "Polygon",
+"coordinates": [[
+[-81.66, 30.33],
+[-81.64, 30.33],
+[-81.64, 30.31],
+[-81.66, 30.31],
+[-81.66, 30.33]
+]]
+}
+},
+{
+"type": "Feature",
+"properties": { "name": "Riverside" },
+"geometry": {
+"type": "Polygon",
+"coordinates": [[
+[-81.69, 30.32],
+[-81.67, 30.32],
+[-81.67, 30.30],
+[-81.69, 30.30],
+[-81.69, 30.32]
+]]
+}
+},
+{
+"type": "Feature",
+"properties": { "name": "San Marco" },
+"geometry": {
+"type": "Polygon",
+"coordinates": [[
+[-81.65, 30.30],
+[-81.63, 30.30],
+[-81.63, 30.28],
+[-81.65, 30.28],
+[-81.65, 30.30]
+]]
+}
+}
+]
+};
